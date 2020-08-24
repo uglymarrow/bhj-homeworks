5a75486f135180be76e7fa7f235468df34cb0b20
Ksenia Yaroshenko <ksus2000@yandex.ru>
2020-08-24 12:59:15 +0300

    Правка задания 1.2 меню

--- element-search/menu/task.js
+++ element-search/menu/task.js
@@ -1,15 +1,20 @@
-const menuItems = document.getElementsByClassName("menu__link");
+const menuLinks = Array.from(document.getElementsByClassName("menu__link"));
 
-Array.from(menuItems).forEach((menuItem) => {
-    menuItem.onclick = () => {
+menuLinks.forEach((menuLink) => {
+    menuLink.onclick = () => {
+        const submenu = menuLink.parentNode.querySelector("ul");
+        if (submenu.className == "menu menu_sub menu_active") {
+            submenu.className = "menu menu_sub";
+            return false;
+        }
+
         const openedMenuItems = Array.from(document.getElementsByClassName("menu menu_sub menu_active"));
         if (openedMenuItems.length) 
             openedMenuItems.forEach((element) => {
                 element.className = "menu menu_sub";
             })
-            
-        const parentNode = menuItem.closest("li");
-        parentNode.querySelector("ul").className = "menu menu_sub menu_active";
+
+        submenu.className = "menu menu_sub menu_active";
         return false;
     }
 })
