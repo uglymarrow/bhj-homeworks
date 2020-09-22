241241c7108869b95dbd1d1b16ee7e6349577519
Ksenia Yaroshenko <ksus2000@yandex.ru>
2020-09-22 11:28:45 +0300

    Внесение поправок

--- document-structure/cart/task.js
+++ document-structure/cart/task.js
@@ -1,24 +1,22 @@
 const addButtons = Array.from(document.getElementsByClassName("product__add"));
-let cart = document.getElementsByClassName("cart__products")[0];
-let productsInCart = [];
+let cart = document.querySelector(".cart__products");
 
 addButtons.forEach(addButton => {
     addButton.addEventListener('click', event => {
         const product = event.currentTarget.closest('.product');
         const productId = product.dataset.id;
         const productCount = product.querySelector('.product__quantity-value').innerText;
-        
-        if (! productsInCart.includes(productId)) {
+        let cartProducts = Array.from(document.querySelectorAll('.cart__product'));
+
+        if (Array.from(cartProducts).every(element =>  element.dataset.id != productId)) {
             const productImg = product.querySelector('.product__image').getAttribute('src');
             cart.insertAdjacentHTML('afterbegin',
                 `<div class="cart__product" data-id="${productId}">
                     <img class="cart__product-image" src=${productImg}>
                     <div class="cart__product-count">${Number(productCount)}</div>
                 </div>`);
-            productsInCart.push(productId);
         } else {
-            let selectedProducts = Array.from(cart.querySelectorAll('.cart__product'));
-            let neededProduct = selectedProducts.find((selectedProduct) => {
+            let neededProduct = cartProducts.find((selectedProduct) => {
                         if (selectedProduct.dataset.id == productId)
                             return true;
                     });
@@ -34,6 +32,7 @@ const incControls = Array.from(document.getElementsByClassName("product__quantit
 
 incControls.forEach(incControl => {
     incControl.addEventListener('click', event => {
+        let count = incControl.parentElement.querySelector(".product__quantity-value");
         incControl.parentElement.querySelector(".product__quantity-value").textContent ++;
     })
 });
@@ -43,9 +42,7 @@ const decControls = Array.from(document.getElementsByClassName("product__quantit
 decControls.forEach(decControl => {
     decControl.addEventListener('click', event => {
         let count = decControl.parentElement.querySelector(".product__quantity-value");
-        if (Number(count.textContent) == 1) 
-            alert('Нельзя добавить в корзину меньше одного товара');
-        else 
+        if (Number(count.textContent) != 1) 
             count.textContent --;
-    })
+    });
 });
--- document-structure/todo/task.js
+++ document-structure/todo/task.js
@@ -1,9 +1,19 @@
 let taskInput = document.getElementById('task__input');
 let tasksList = document.getElementById('tasks__list');
+let addButton = document.getElementById('tasks__add');
 
 document.onkeydown = event => {
     if ((event.key == "Enter") && (taskInput.value != '')) {
-        tasksList.insertAdjacentHTML('afterbegin',
+        addTask();
+        return false;
+    };
+    return true;
+};
+
+addButton.addEventListener('click', event => addTask());
+
+function addTask() {
+  tasksList.insertAdjacentHTML('afterbegin',
             `<div class="task">
               <div class="task__title">
                 ${taskInput.value}
@@ -11,12 +21,9 @@ document.onkeydown = event => {
               <a href="#" class="task__remove">&times;</a>
             </div>`);
 
-        let taskRemover = tasksList.firstElementChild.querySelector('.task__remove'); 
-        taskRemover.addEventListener('click', (event) => {
+  let taskRemover = tasksList.firstElementChild.querySelector('.task__remove'); 
+  taskRemover.addEventListener('click', (event) => {
             event.currentTarget.parentElement.remove();
-        });   
-        return false;
-    };
-    return true;
-};
-
+        });  
+  taskInput.value = ''; 
+}
